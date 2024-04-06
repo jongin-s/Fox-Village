844d3497e43eee3b2d0e8d68250f52476c1132e2
bizonsmg <theturningpoint798@gmail.com>
2024-04-06 22:56:11 +0900

    카메라 드래그를 구현

---
+++ Assets/Scripts/FixedTouchField.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHandler  // 터치 포인터 핸들러를 가져와야 함
+{
+    [HideInInspector] public Vector2 TouchDist;  // 터치 드래그의 길이를 나타내는 2차원 벡터
+    [HideInInspector] public Vector2 PointerOld;  // 처음에 터치 드래그를 시작했던 위치
+    [HideInInspector] protected int PointerId;  // 터치 포인터의 이름
+    [HideInInspector] public bool Pressed;  // 터치 상태를 나타내는 불리언 값
+
+    void Update()
+    {
+        if (Pressed)
+        {
+            if (PointerId >= 0 && PointerId < Input.touches.Length)  // 터치 포인터가 존재하고 터치 드래그의 길이보다 짧을 때
+            {
+                TouchDist = Input.touches[PointerId].position - PointerOld;
+                PointerOld = Input.touches[PointerId].position;  // TouchDist는 원래 포지션 PointerOld와 새 포지션 사이의 거리 벡터
+            }
+            else
+            {
+                TouchDist = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - PointerOld;
+                PointerOld = Input.mousePosition;  // 포인터를 처음 생성할 때 실행
+            }
+        }
+        else { TouchDist = new Vector2(); }  // 터치 상태가 아닐 때 TouchDist를 초기화
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+        PointerId = eventData.pointerId;
+        PointerOld = eventData.position;  // 터치를 할 때 Pressed를 true로 설정하고 터치 포인터와 그 위치를 생성
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     Animator anim;  // Animator 클래스의 메소드
     Vector3 moveVec;  // 3차원 벡터 변수
     RunButton runButton;  // RunButton 클래스의 메소드
+    [SerializeField] Transform cam;  // Transform 클래스의 메소드 (카메라)
 
     private void Start()
     {
@@ -22,20 +23,19 @@ public class Player : MonoBehaviour
         anim.SetBool("isWalk", moveVec != Vector3.zero);  // 애니메이션은 걷기가 기본값, 이동 벡터가 0이 아니라면 걷기
         anim.SetBool("isRun", runButton.click);  // runButton을 누르고 있는 동안 달리기
 
-        float x = joy.Horizontal;
-        float z = joy.Vertical;  // 조이스틱의 가로와 세로 방향 좌표는 각각 x축과 z축 움직임으로 치환
+        float x = joy.Horizontal; float z = joy.Vertical;  // 조이스틱의 가로와 세로 방향 좌표는 각각 x축과 z축 움직임으로 치환
+        Vector3 camForward = cam.forward; Vector3 camRight = cam.right;  // 카메라의 상대적인 방향벡터
+        camForward.y = 0; camRight.y = 0;  // 카메라의 y축 방행벡터는 사용하지 않을 것이므로 0으로 설정
 
-        moveVec = new Vector3(x, 0, z) * speed * Time.deltaTime * (runButton.click ? 2 : 1);  // 이동 벡터에 반드시 속도와 deltaTime을 곱해줘야 함
+        Vector3 forwardRelative = z * camForward.normalized; Vector3 rightRelative = x * camRight;  // 카메라의 forward를 z축, right를 x축의 움직임과 곱함 (카메라가 높은 각도에 있으면, 즉 카메라의 y축 절댓값이 크면 camForward의 값이 작아지고 플레이어의 조이스틱 세로 이동이 느려지는 현상 발생, 이를 해결하기 위해 camForward에 벡터의 값이 항상 1이 되게 하는 normalized 메소드를 사용하면 camForward = (0, -1, 0)이 아닌 이상 문제가 발생하지 않음)
+        Vector3 moveDir = forwardRelative + rightRelative;  // 조이스틱을 움직일 때 플레이어가 실제로 움직일 방향은 카메라의 상대적인 방향에 의해 결정
+
+        moveVec = new Vector3(moveDir.x, 0, moveDir.z) * speed * Time.deltaTime * (runButton.click ? 2 : 1);  // 이동 벡터에 반드시 속도와 deltaTime을 곱해줘야 함
         rigid.MovePosition(rigid.position + moveVec);  // Rigidbody의 원래 포지션에서 이동 벡터를 더해서 움직임
         if (moveVec.sqrMagnitude == 0) return;  // sqrMagnitude는 벡터의 각 값의 제곱의 합, 이동 벡터가 0이면 리턴
 
         Quaternion dirQuat = Quaternion.LookRotation(moveVec);
         Quaternion moveQuat = Quaternion.Slerp(rigid.rotation, dirQuat, 0.3f);
         rigid.MoveRotation(moveQuat);  // Quaternion 클래스는 3차원 벡터값과 회전값을 묶은 4차원 튜플, 매끄러운 회전 구현
-    }
-
-    private void LateUpdate()
-    {
-        // anim.SetFloat("Move", moveVec.sqrMagnitude);
     }
 }
