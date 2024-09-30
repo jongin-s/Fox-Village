9eefff3446f089561dc31a0c09757d18ad0736db
bizonsmg <theturningpoint798@gmail.com>
2024-09-30 13:29:53 +0900

    옵션 메뉴 조정, 몬스터 스케일 조정

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -8,41 +8,6 @@ using TMPro;
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer mixer;
-    public TMP_Dropdown resolutionDropdown;
-
-    Resolution[] resolutions;
-
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void Ratio(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, true);
-    }
 
     public void Close()
     {
