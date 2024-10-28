0686a6055939f8c76d5a6fce11020bfc38bd46ce
JongIn Kim <ppj7515@gmail.com>
2024-10-28 22:51:47 +0900

    터치 이펙트 추가

---
+++ Assets/Scripts/ParticleSpawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class UITouchParticleSpawner : MonoBehaviour
+{
+    public ParticleSystem particlePrefab; // UI용 파티클 프리팹
+    public Canvas canvas; // 파티클을 올릴 Canvas
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 터치나 클릭 감지
+        {
+            Vector3 touchPosition = Input.mousePosition; // 터치 위치
+            touchPosition.z = 0f;
+
+            ParticleSystem particle = Instantiate(particlePrefab, touchPosition, Quaternion.identity, canvas.transform); // 캔버스 안에 파티클 생성
+            particle.transform.localScale = new Vector3(300, 300, 300); // 크기를 300으로 설정
+            particle.gameObject.layer = LayerMask.NameToLayer("UI"); // "UI" 레이어로 설정
+            particle.Play(); // 파티클 재생
+
+            Destroy(particle.gameObject, particle.main.duration); // 지속 시간 후 파괴
+        }
+    }
+}
