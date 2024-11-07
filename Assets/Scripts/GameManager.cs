afa79da125ca329620019752df9683c3eac25bb6
JongIn Kim <ppj7515@gmail.com>
2024-11-07 12:25:40 +0900

    코인을 보석으로 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
 
     public void Awake()
     {
-        invTxt0 = GameObject.Find("Slot (0)").GetComponentInChildren<TextMeshProUGUI>();  // 구체적인 슬롯 이름에서 자식의 컴포넌트를 가져오지 않으면 다른 "Text (TMP)" 오브젝트가 업데이트됨
+        invTxt0 = GameObject.Find("Jewel").GetComponentInChildren<TextMeshProUGUI>();  // 구체적인 슬롯 이름에서 자식의 컴포넌트를 가져오지 않으면 다른 "Text (TMP)" 오브젝트가 업데이트됨
     }
 
     public void GetItem(TextMeshProUGUI invTxt, int count)
